1adcfa8c6e2e50aea7b5c1adf53bdb9ec0fd7c28
mobpsychie8-gif <mobpsychie8@gmail.com>
2026-04-21 18:39:19 +0530

    Delete frontend directory

--- frontend/src/lib/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const API = axios.create({ baseURL: "/api" });
-API.interceptors.request.use(c => {
-  const t = localStorage.getItem("token");
-  if (t) c.headers.Authorization = "Bearer " + t;
-  return c;
-});
-export default API;
